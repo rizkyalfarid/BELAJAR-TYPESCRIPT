a4d1519653c72379d21e05895ac22c9e69ef882b
Rizky Al Farid Hafizh <112795290+rizkyalfarid@users.noreply.github.com>
2024-05-05 22:09:25 +0700

    Update array.ts

--- array.ts
+++ array.ts
@@ -1,21 +1,21 @@
 const arrayAngka : number[] = [];
 
-// let angka : number[] = [1,3,2,7,6,5,9,8,10,4];
+ let angka : number[] = [1,3,2,7,6,5,9,8,10,4];
 
-// arrayAngka.push(...angka);
+ arrayAngka.push(...angka);
 
-// for(let i : number = 0; i < angka.length; i++) {
-//   for(let j : number = 0; j < angka.length - 1; j++) {
-//     if(arrayAngka[j] > arrayAngka[j + 1]) {
-//       let temp : number;
-//       temp = arrayAngka[j]
-//       arrayAngka[j] = arrayAngka[j + 1];
-//       arrayAngka[j + 1] = temp;
-//     }
-//   }
-// }
+ for(let i : number = 0; i < angka.length; i++) {
+   for(let j : number = 0; j < angka.length - 1; j++) {
+     if(arrayAngka[j] > arrayAngka[j + 1]) {
+       let temp : number;
+       temp = arrayAngka[j]
+       arrayAngka[j] = arrayAngka[j + 1];
+       arrayAngka[j + 1] = temp;
+     }
+   }
+ }
 
-// console.log(arrayAngka);
+console.log(arrayAngka);
 
 let namaMahasiswa : string[] = []; 
 
